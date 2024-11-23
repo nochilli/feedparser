3d7102f5e306c85a6d888a43874dfdf4ec662381
Suraj N R <ricemms10@gmail.com>
2024-11-24 00:16:31 +0530

    2

--- settings.py
+++ settings.py
@@ -16,10 +16,10 @@ whitelist = (
     "talker",
     "demon", 
     "ranma"
-)
-username = "Episode Update"
-webhook = "https://discord.com/api/webhooks/1309924781067468873/zVB3GtFkN5o00sANZ38BTaxtcZHu2Eehy2H3jZdTf-5DtOQuII9Wze4naNJEPyeMpM_G"
+);
+username = "Episode Update";
+webhook = "https://discord.com/api/webhooks/1309924781067468873/zVB3GtFkN5o00sANZ38BTaxtcZHu2Eehy2H3jZdTf-5DtOQuII9Wze4naNJEPyeMpM_G";
 avatar_url = (
     "https://source.android.com/static/docs/setup/images/Android_symbol_green_RGB.png"
-)
-feed_url = "https://morss.it/:items=||*[class=name]/https://anikoto.to/filter?keyword=&type=&country%5B%5D=6&term_type%5B%5D=11&status%5B%5D=ongoing&language%5B%5D=sub&sort=latest-updated"
+);
+feed_url = "https://morss.it/:items=||*[class=name]/https://anikoto.to/filter?keyword=&type=&country%5B%5D=6&term_type%5B%5D=11&status%5B%5D=ongoing&language%5B%5D=sub&sort=latest-updated";
